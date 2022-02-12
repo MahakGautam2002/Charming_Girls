fb2a7ab20869007a0a673fae4030bb742c17cff0
Mahak Gautam <87615347+MahakGautam2002@users.noreply.github.com>
2022-02-12 17:00:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
+from tkinter import *
 import pandas as pd
 import csv
 from tabulate import tabulate
 
+window=Tk()
+window.title("main")
 data=pd.read_csv("hospital_directory.csv")
 
 dict=data.to_dict()
-print(tabulate(data, headers = 'keys', tablefmt = 'psql'))
-
+ft=tabulate(data.head(), headers = 'keys', tablefmt = 'simple',colalign=("right",))
+print(ft)
+lable1=Label(text=ft)
+lable1.pack()
 
 list=[]
+
+
 input_pin=input("Enter Pincode:")
+
+lable0=Label(text="Result::",font=("Arial",40,"bold"))
+lable0.pack()
+
 with open("hospital_directory.csv","r") as file:
     d_reader=csv.DictReader(file)
     for i in d_reader:
+        
         #print("Found Entry :")
         if i["Pincode"]==input_pin:
             list.append(i)
             #print(i)
-tab=pd.DataFrame(list,columns=["Hospital_Name","Location","State","Telephone","Address_Original_First_Line"])
+#tab=pd.DataFrame(list,columns=["Hospital_Name","Location","State","Telephone","Address_Original_First_Line"])
+tab=tabulate(list, headers = 'keys', tablefmt = 'grid',colalign=("right",))
 print(tab)
+
+lable2=Label(text=tab)
+lable2.pack()
+window.mainloop()
 
